e468be88684d9592c80ca248b5c9f27be5ed3c80
aden-q <627374286@qq.com>
2022-01-31 19:38:43 -0600

    Create reverse_to_make_equal.py

---
+++ code/reverse_to_make_equal.py
@@ -0,0 +1,74 @@
+import math
+# Add any extra import statements you may need here
+
+
+# Add any helper functions you may need here
+
+
+def are_they_equal(array_a, array_b):
+  # Write your code here
+  dict_a = {}
+  dict_b = {}
+  for c in array_a:
+    if c not in dict_a.keys():
+      dict_a[c] = 1
+    else:
+      dict_a[c] += 1
+  for c in array_b:
+    if c not in dict_b.keys():
+      dict_b[c] = 1
+    else:
+      dict_b[c] += 1
+  
+  return dict_a == dict_b
+
+
+
+
+
+
+
+
+
+
+# These are the tests we use to determine if the solution is correct.
+# You can add your own at the bottom.
+def printString(string):
+  print('[\"', string, '\"]', sep='', end='')
+
+test_case_number = 1
+
+def check(expected, output):
+  global test_case_number
+  result = False
+  if expected == output:
+    result = True
+  rightTick = '\u2713'
+  wrongTick = '\u2717'
+  if result:
+    print(rightTick, 'Test #', test_case_number, sep='')
+  else:
+    print(wrongTick, 'Test #', test_case_number, ': Expected ', sep='', end='')
+    printString(expected)
+    print(' Your output: ', end='')
+    printString(output)
+    print()
+  test_case_number += 1
+
+if __name__ == "__main__":
+  n_1 = 4
+  a_1 = [1, 2, 3, 4]
+  b_1 = [1, 4, 3, 2]
+  expected_1 = True
+  output_1 = are_they_equal(a_1, b_1)
+  check(expected_1, output_1)
+
+  n_2 = 4
+  a_2 = [1, 2, 3, 4]
+  b_2 = [1, 2, 3, 5]  
+  expected_2 = False
+  output_2 = are_they_equal(a_2, b_2)
+  check(expected_2, output_2)
+
+  # Add your own test cases here
+  
