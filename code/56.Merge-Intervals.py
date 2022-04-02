79fb03b2c9a2a703c607044ae0d71474842f5c40
aden-q <627374286@qq.com>
2022-04-02 00:32:49 -0500

    Update

---
+++ code/56.Merge-Intervals.py
@@ -0,0 +1,18 @@
+class Solution:
+    def merge(self, intervals: List[List[int]]) -> List[List[int]]:
+        res = []
+        # sort by the left end point, acendingly
+        intervals.sort(key = lambda x: x[0])
+        left = intervals[0][0]
+        right = intervals[0][1]
+        for i in range(1, len(intervals)):
+            if intervals[i][0] <= right:
+                # merge
+                right = max(right, intervals[i][1])
+            else:
+                res.append([left, right])
+                # upate new interval range
+                left = intervals[i][0]
+                right = intervals[i][1]
+        res.append([left, right])
+        return res
