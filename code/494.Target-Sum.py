6f022b905735e0b1cf21f08387284a4b900de8b8
aden-q <627374286@qq.com>
2022-04-02 21:11:16 -0500

    Update

---
+++ code/494.Target-Sum.py
@@ -0,0 +1,13 @@
+class Solution:
+    def findTargetSumWays(self, nums: List[int], target: int) -> int:
+        sumNums = sum(nums)
+        if target > sumNums or target < -sumNums:
+            return 0
+        if (target + sumNums) % 2 == 1:
+            return 0
+        dp = [0] * ((target + sumNums) // 2 + 1)
+        dp[0] = 1
+        for i in range(len(nums)):
+            for j in range((target + sumNums) // 2, nums[i] - 1, -1):
+                dp[j] += dp[j - nums[i]]
+        return dp[-1]
