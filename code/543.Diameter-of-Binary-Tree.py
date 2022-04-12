4959ce1f1139e8f8cf45d06fab93f76f9e44db2b
aden-q <627374286@qq.com>
2022-04-11 22:35:13 -0500

    Update

---
+++ code/543.Diameter-of-Binary-Tree.py
@@ -0,0 +1,19 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def diameterOfBinaryTree(self, root: Optional[TreeNode]) -> int:
+        diameter = 0
+        def maxDepth(root):
+            nonlocal diameter
+            if not root:
+                return 0
+            max_depth_left = maxDepth(root.left)
+            max_depth_right = maxDepth(root.right)
+            diameter = max(diameter, max_depth_left + max_depth_right)
+            return max(max_depth_left, max_depth_right) + 1
+        maxDepth(root)
+        return diameter
