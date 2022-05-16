e9de8a414c0c1b8ca378a1da800fcceb46035fad
aden-q <qian0102@umn.edu>
2022-05-16 16:10:29 -0500

    Update

---
+++ code/88.Merge-Sorted-Array.py
@@ -0,0 +1,14 @@
+class Solution:
+    def duplicateZeros(self, arr: List[int]) -> None:
+        """
+        Do not return anything, modify arr in-place instead.
+        """
+        i = 0
+        while i < len(arr) - 1:
+            if arr[i] != 0:
+                i += 1
+                continue
+            for j in range(len(arr) - 1, i, -1):
+                arr[j] = arr[j-1]
+            arr[i+1] = 0
+            i += 2
