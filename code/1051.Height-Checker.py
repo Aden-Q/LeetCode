77bb561ec0f4e8013308bc3343c5ef613e0aa31e
aden-q <qian0102@umn.edu>
2022-05-16 17:01:08 -0500

    Update

---
+++ code/1051.Height-Checker.py
@@ -0,0 +1,8 @@
+class Solution:
+    def heightChecker(self, heights: List[int]) -> int:
+        sorted_heights = sorted(heights)
+        count = 0
+        for i in range(len(heights)):
+            if heights[i] != sorted_heights[i]:
+                count += 1
+        return count
