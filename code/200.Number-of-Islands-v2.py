6f5c97c1773f679a706203cd9704a5d9f514e084
aden-q <qian0102@umn.edu>
2022-05-16 21:46:54 -0500

    Create 200.Number-of-Islands-v2.py

---
+++ code/200.Number-of-Islands-v2.py
@@ -0,0 +1,25 @@
+class Solution:
+    def numIslands(self, grid: List[List[str]]) -> int:
+        m = len(grid)
+        n = len(grid[0])
+        
+        def dfs(r, c):
+            nonlocal grid, m, n
+            if grid[r][c] == '1':
+                grid[r][c] = '0'
+                if r - 1 > -1:
+                    dfs(r-1, c)
+                if r + 1 < m:
+                    dfs(r+1, c)
+                if c - 1 > -1:
+                    dfs(r, c-1)
+                if c + 1 < n:
+                    dfs(r, c+1)
+        
+        count = 0
+        for i in range(m):
+            for j in range(n):
+                if grid[i][j] == '1':
+                    count += 1
+                    dfs(i, j)
+        return count
