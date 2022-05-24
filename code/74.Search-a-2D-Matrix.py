f9e21e12e2d5cf9520c0702d9e3e0e8e19f936fd
aden-q <qian0102@umn.edu>
2022-05-23 22:24:36 -0500

    Update

---
+++ code/74.Search-a-2D-Matrix.py
@@ -0,0 +1,19 @@
+class Solution:
+    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+        m = len(matrix) # 1
+        n = len(matrix[0]) # 2
+        left = 0
+        right = m * n - 1 # 1
+        
+        while left <= right:
+            mid = (left + right) // 2
+            row = mid // n
+            col = mid % n 
+            if matrix[row][col] == target:
+                return True
+            elif matrix[row][col] < target:
+                left = mid + 1
+            else:
+                right = mid - 1
+                
+        return False
