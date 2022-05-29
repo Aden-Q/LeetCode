edab5818c188da7ed6a66a92d849de82f25667ff
aden-q <qian0102@umn.edu>
2022-05-29 01:25:15 -0500

    Update

---
+++ code/1678.Goal-Parser-Interpretation.py
@@ -0,0 +1,16 @@
+class Solution:
+    def interpret(self, command: str) -> str:
+        res = []
+        i = 0
+        while i < len(command):
+            if command[i] == 'G':
+                res.append('G')
+                i += 1
+            elif command[i] == '(':
+                if command[i+1] == ')':
+                    res.append('o')
+                    i += 2
+                else:
+                    res.append('al')
+                    i += 4
+        return ''.join(res)
