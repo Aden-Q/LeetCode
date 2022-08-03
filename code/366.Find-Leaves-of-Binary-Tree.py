41010713f821d5a96845cfa4a46f3b4fccc0658b
Zecheng Qian <zecheng.qian@pantheon.io>
2022-08-03 14:38:26 -0500

    Update 366.Find-Leaves-of-Binary-Tree.py

--- code/366.Find-Leaves-of-Binary-Tree.py
+++ code/366.Find-Leaves-of-Binary-Tree.py
@@ -6,16 +6,17 @@
 #         self.right = right
 class Solution:
     def findLeaves(self, root: Optional[TreeNode]) -> List[List[int]]:
-        self.res = [[] for _ in range(100)]
-        self.max_height = 0
+        self.res = []
         
         def traverse(root):
             if not root:
                 return -1
             cur_height = max(traverse(root.left), traverse(root.right)) + 1
-            self.max_height = max(self.max_height, cur_height)
-            self.res[cur_height].append(root.val)
+            if cur_height + 1 > len(self.res):
+                self.res.append([root.val])
+            else:
+                self.res[cur_height].append(root.val)
             return cur_height
         
         traverse(root)
-        return self.res[:self.max_height + 1]
+        return self.res
