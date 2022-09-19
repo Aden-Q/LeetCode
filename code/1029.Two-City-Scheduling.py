0bb77291a20c88e4ef430157be082ebcd2801faf
Zecheng Qian <zecheng.qian@pantheon.io>
2022-09-19 00:03:45 -0500

    Create 1029.Two-City-Scheduling.py

---
+++ code/1029.Two-City-Scheduling.py
@@ -0,0 +1,9 @@
+class Solution:
+    def twoCitySchedCost(self, costs: List[List[int]]) -> int:
+        costs.sort(key = lambda x : x[0] - x[1])
+        total_cost = 0
+        
+        for i in range(len(costs) // 2):
+            total_cost += costs[i][0] + costs[i + len(costs) // 2][1]
+            
+        return total_cost
