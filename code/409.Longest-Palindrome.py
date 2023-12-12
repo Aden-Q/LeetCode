55a16d85c496007dd71455cd368eb19b2499bcdb
Aden-q <qian0102@umn.edu>
2023-12-12 14:29:36 -0600

    docs: optimize 409

--- code/409.Longest-Palindrome.py
+++ code/409.Longest-Palindrome.py
@@ -3,15 +3,8 @@ class Solution:
         c = collections.Counter(s)
         res = 0
 
-        flag = False
         for key in c:
-            if c[key] % 2 == 0:
-                res += c[key]
-            elif flag:
-                res += c[key] - 1
-            else:
-                res += c[key]
-                flag = True
+            res += c[key] // 2 * 2
         
-        return res
+        return res + 1 if res < len(s) else res
         
