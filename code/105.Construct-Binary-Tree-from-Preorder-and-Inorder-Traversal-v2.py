c456f7a5f7e87adb3cfa07d208491a5890db8316
Aden-q <qian0102@umn.edu>
2023-12-20 17:23:59 -0600

    docs: add 323

--- code/105.Construct-Binary-Tree-from-Preorder-and-Inorder-Traversal-v2.py
+++ code/105.Construct-Binary-Tree-from-Preorder-and-Inorder-Traversal-v2.py
@@ -1,35 +1,29 @@
-# Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
 class Solution:
-    def buildTree(self, preorder: List[int], inorder: List[int]) -> Optional[TreeNode]:
+    def countComponents(self, n: int, edges: List[List[int]]) -> int:
+        # build the graph as an adjacency list
+        graph = [[] for _ in range(n)]
+        visited = [False] * n
+
+        for edge in edges:
+            node1, node2 = edge
+            graph[node1].append(node2)
+            graph[node2].append(node1)
+
+        def dfs(node):
+            if visited[node]:
+                return
+
+            visited[node] = True
+            for next_node in graph[node]:
+                dfs(next_node)
+            
+            return
+
+        cnt = 0
+        for node in range(n):
+            if visited[node]:
+                continue
+            dfs(node)
+            cnt += 1
         
-        def dfs(pre_start, pre_end, in_start, in_end):
-            # base case, when nothing to build
-            if pre_end < pre_start:
-                return None
-            
-            # build left subtree and right subtree recursively
-            pivot = preorder[pre_start]
-            index = inorder.index(pivot)
-            left_in_start = in_start
-            left_in_end = index - 1
-            left_length = index - in_start
-            right_in_start = index + 1
-            right_in_end = in_end
-            right_length = in_end - index
-
-            left_pre_start = pre_start + 1
-            left_pre_end = left_pre_start + left_length - 1
-            right_pre_start = left_pre_end + 1
-            right_pre_end = right_pre_start + right_length - 1
-            
-            curr = TreeNode(pivot)
-            curr.left = dfs(left_pre_start, left_pre_end, left_in_start, left_in_end)
-            curr.right = dfs(right_pre_start, right_pre_end, right_in_start, right_in_end)
-            return curr
-
-        return dfs(0, len(preorder) - 1, 0, len(inorder) - 1)
+        return cnt
