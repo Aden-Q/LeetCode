288d71f434e9d1e7dd5df8b0e2c648312e566a7d
Aden-q <qian0102@umn.edu>
2023-12-24 19:22:05 -0600

    docs: add 1493

--- code/1493.Longest-Subarray-of-1's-After-Deleting-One-Element.py
+++ code/1493.Longest-Subarray-of-1's-After-Deleting-One-Element.py
@@ -1,13 +1,6 @@
 class Solution:
     def longestSubarray(self, nums: List[int]) -> int:
-        # this is an edge case
-        total_sum = sum(nums)
-        if total_sum == 0:
-            return 0
-        if total_sum == len(nums):
-            return total_sum - 1
-
-        max_len = 1
+        max_len = 0
         # count the number of zeros in the subarray
         # we can have at most 1 zero
         cnt_zero = 0
@@ -20,7 +13,7 @@ class Solution:
                     cnt_zero -= 1
                 left += 1
             
-            max_len = max(max_len, right - left + 1 - cnt_zero)
+            max_len = max(max_len, right - left)
             right += 1
 
         return max_len
