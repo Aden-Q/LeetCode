4de8c56ec94c2a5af4d8227783132158487f9c19
Aden-q <qian0102@umn.edu>
2023-12-29 09:39:20 -0600

    docs: add 123

---
+++ code/123.Best-Time-to-Buy-and-Sell-Stock-III.py
@@ -0,0 +1,19 @@
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        n = len(prices)
+        # a dp state can be described by 3 variables:
+        # day: the day we are current at, 0-indexed for convenience
+        # hold: whether we are holding the stock or not on the current day
+        # k: number of transactions left to buy and sell stocks
+        # the function returns the maximum profit we can get for the given state
+        prev_day = [[0] * 3 for _ in range(2)]
+        curr_day = [[0] * 3 for _ in range(2)]
+        for day in reversed(range(n)):
+            for k_iter in range(1, 3):
+                # when hold == False
+                curr_day[0][k_iter] = max(-prices[day] + prev_day[1][k_iter], prev_day[0][k_iter])
+                # when hold == True
+                curr_day[1][k_iter] = max(prev_day[1][k_iter], prices[day] + prev_day[0][k_iter-1])
+            prev_day, curr_day = curr_day, prev_day
+
+        return prev_day[0][2]
