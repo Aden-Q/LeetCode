66e9426279f7ab5101b5143a98b805ee565c78da
Aden-q <qian0102@umn.edu>
2024-02-12 18:37:13 -0600

    docs: add 422 improved

--- code/422.Valid-Word-Square.py
+++ code/422.Valid-Word-Square.py
@@ -1,8 +1,7 @@
 class Solution:
     def validWordSquare(self, words: List[str]) -> bool:
         for row in range(len(words)):
-            word = words[row]
-            for col in range(len(word)):
+            for col in range(len(words[row])):
                 if col >= len(words) or row >= len(words[col]) or words[col][row] != words[row][col]:
                     return False
         
