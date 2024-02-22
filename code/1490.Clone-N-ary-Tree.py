a7fdb835a813ed9ffe8dd263e94acb8cb586d87c
Aden-q <qian0102@umn.edu>
2024-02-21 20:52:31 -0600

    docs: add 1490

---
+++ code/1490.Clone-N-ary-Tree.py
@@ -0,0 +1,18 @@
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val=None, children=None):
+        self.val = val
+        self.children = children if children is not None else []
+"""
+
+class Solution:
+    def cloneTree(self, root: 'Node') -> 'Node':
+        if not root:
+            return root
+
+        node = Node(root.val)
+        for child in root.children:
+            node.children.append(self.cloneTree(child))
+
+        return node
